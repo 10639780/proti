03a3fc5411a63eb84529e1a3770b786b9eca6c29
Xam Seymonson <xammy_697@hotmail.com>
2020-06-06 01:30:11 +0200

    testjes.py for analysing all proteins in one go, gets stuck often though :(

--- testjes.py
+++ testjes.py
@@ -81,10 +81,11 @@ def atom_positions(proteins):
             if len(moves) == 0:
                 print("Protein is stuck.")
                 # would like to change this so if 1 protein stuck it does continue for the other proteins in the file
-                sys.exit()
+                # sys.exit()
+                break
                 
-
-            current_position = moves[randint(0, len(moves) - 1)]
+            best_moves = lowest_score(moves)
+            current_position = best_moves[randint(0, len(best_moves) - 1)]
             total_score += current_position[2]
             
             grid[current_position[0]][current_position[1]] = atom
@@ -92,7 +93,9 @@ def atom_positions(proteins):
             log_y.append(current_position[1])
       
         proteins[key][2] = grid
-        proteins[key].append(total_score)       
+        proteins[key].append(total_score) 
+        proteins[key].append(log_x)  
+        proteins[key].append(log_y)
 
     return proteins
 
@@ -100,35 +103,16 @@ def possible_moves(x, y, _type, grid):
     """Determines which moves are possible from a given position. 
     If possible function will also determine the points gained by making that move."""
 
-    # list to hold all valid moves
     moves = []
-   
+    directions = [[x-1,y], [x,y+1], [x+1,y], [x,y-1]]
+
     # check if the target tile is still empty, and whether that tile doesn't have 4 filled neighbours already, which would result in the string getting wrappped into itself
-    if grid[x-1][y] == 0 and neighbour_count(x-1, y, grid) < 4:
-        score = 0
-        # if the protein to be placed is hydrophobe, determine what the score would be if placed in this position
-        if _type == 'H':
-            score = points(x-1, y, x, y, grid)
-        # add to list of valid moves
-        moves.append([x-1, y, score])
-    
-    if grid[x][y+1] == 0 and neighbour_count(x, y+1, grid) < 4:
-        score = 0
-        if _type == 'H':
-            score = points(x, y+1, x, y, grid)
-        moves.append([x, y+1, score])
-
-    if grid[x+1][y] == 0 and neighbour_count(x+1, y, grid) < 4:
-        score = 0
-        if _type == 'H':
-            score = points(x+1, y, x, y, grid)
-        moves.append([x+1, y, score])
-
-    if grid[x][y-1] == 0 and neighbour_count(x, y-1, grid) < 4:
-        score = 0
-        if _type == 'H':
-            score = points(x, y-1, x, y, grid)
-        moves.append([x, y-1, score])
+    for direction in directions:
+        if grid[direction[0]][direction[1]] == 0 and neighbour_count(direction[0], direction[1], grid) < 4:
+            # calculate points gained by making this move
+            score = points(direction[0], direction[1], x, y, _type, grid)
+            # place move and associated score in the moves list
+            moves.append([direction[0],direction[1], score])
 
     return moves
 
@@ -136,40 +120,66 @@ def neighbour_count(x, y, grid):
     """Counts how many non-empty neighbours a given position has."""
     counter = 0
 
-    # goes to all 4 neighbouring tiles, if not empty, add 1 to counter
-    if grid[x-1][y] != 0:
-        counter += 1
-    
-    if grid[x][y+1] != 0:
-        counter += 1
+    directions = [[x-1,y], [x,y+1], [x+1,y], [x,y-1]]
 
-    if grid[x+1][y] != 0:
-        counter += 1
-
-    if grid[x][y-1] != 0:
-        counter += 1
+    for direction in directions:
+        # check whether the neighbouring tile is occupied, if so add one to counter
+        if grid[direction[0]][direction[1]] != 0:
+            counter += 1
 
     return counter
 
-def points(x, y, old_x, old_y, grid):
+def points(x, y, old_x, old_y, _type, grid):
     """Determines how many points are gained by making a move to x, y, coming from old_x, old_y."""
     
     counter = 0
 
-    # add 1 point if the target position is a hydrophobe and is not the previous protein in the string
-    if grid[x-1][y] == 'H' and not (x-1 == old_x and y == old_y):
-        counter += 1
-    
-    if grid[x][y+1] == 'H' and not (x == old_x and y+1 == old_y):
-        counter += 1
+    HH_HC_score = -1
+    CC_score = -5
+    directions = [[x-1,y], [x,y+1], [x+1,y], [x,y-1]]
 
-    if grid[x+1][y] == 'H' and not (x+1 == old_x and y == old_y):
-        counter += 1
+    #  add to the score if the target position is occupied by an H or C atom
+    if _type == 'H':
 
-    if grid[x][y-1] == 'H' and not (x == old_x and y-1 == old_y):
-        counter += 1
+        for direction in directions:
+            if (grid[direction[0]][direction[1]] == 'H' or grid[direction[0]][direction[1]] == 'C') and not (direction[0] == old_x and direction[1] == old_y):
+                counter += HH_HC_score            
+
+    # for C atom differentiate between the cases of neighbouring with a C or H atom
+    if _type == 'C':
+
+        for direction in directions:
+            if grid[direction[0]][direction[1]] == 'H' and not (direction[0] == old_x and direction[1] == old_y):
+                counter += HH_HC_score   
+
+        for direction in directions:
+            if grid[direction[0]][direction[1]] == 'C' and not (direction[0] == old_x and direction[1] == old_y):
+                counter += CC_score  
 
     return counter
+
+
+def lowest_score(moves):
+    """Determines which move has te highest score."""
+
+    best_moves = []
+    best_score = 0
+
+    for move in moves:
+        
+        # take the score assiciated with each move
+        score = move[2]
+    
+        # if score is equal to the best add it to the list of best moves
+        if score == best_score:
+            best_moves.append(move)
+        # when a lower score is found, empty the list and place the new score in there
+        elif score < best_score:
+            best_moves = [move]
+            best_score = score 
+        
+    return best_moves
+
 
 def display(proteins):
     """Gives a graphical display of the protein string after folding."""
@@ -181,6 +191,8 @@ def display(proteins):
         red_dots_y = []
         blue_dots_x = []
         blue_dots_y = []
+        yellow_dots_x = []
+        yellow_dots_y = []
 
         grid_size = 2 * proteins[key][1] + 1
         grid = proteins[key][2]
@@ -198,10 +210,20 @@ def display(proteins):
                     blue_dots_x.append(i)
                     blue_dots_y.append(j)
 
+                if grid[i][j] == "C":
+                    yellow_dots_x.append(i)
+                    yellow_dots_y.append(j)
+
         # make a colorcoded graph
-        plt.plot(red_dots_x,red_dots_y,'or',blue_dots_x,blue_dots_y,'ob')
-        plt.title('Folded protein string')
-        plt.text(1, 1, f'Score: {score}')
+        plt.plot(red_dots_x, red_dots_y, 'or', label='Hydrophobe')
+        plt.plot(blue_dots_x, blue_dots_y,'ob', label='Polar')
+        
+        if len(yellow_dots_x) != 0:
+            plt.plot(yellow_dots_x, yellow_dots_y, 'oy', label='Cysteine')
+        plt.plot(proteins[key][4], proteins[key][5],'--')
+        plt.title(f'Folded protein string, score: {score}')
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        
         plt.xlim(0, grid_size)
         plt.ylim(0, grid_size)
         plt.show()
