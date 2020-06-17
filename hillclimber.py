663e9da1fcf8343b8d253d7f331e324a98c0a3e7
Xam Seymonson <xammy_697@hotmail.com>
2020-06-17 19:31:58 +0200

    score functie aangepast

--- hillclimber.py
+++ hillclimber.py
@@ -11,7 +11,7 @@ import copy
 # PROTEIN = 'PPHPPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHPHH'
 PROTEIN = 'CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC'
 LENGTH = len(PROTEIN)
-ITERATIONS = 1000
+ITERATIONS = 100
 DIRECTIONS = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
 
@@ -26,32 +26,35 @@ def protein_configuration():
     best_y = []
     scores = []
 
+    score = 0
     while rotations < ITERATIONS:
 
         backup_x = copy.deepcopy(x)
         backup_y = copy.deepcopy(y)
 
+        old_score = copy.deepcopy(score)
+
         rotating_amino = random.randint(0, LENGTH - 1)
-        fold_protein(x, y, rotating_amino)
+        score = fold_protein(x, y, rotating_amino)
 
         if double(x, y):
             x = backup_x
             y = backup_y
             continue
 
-        old_score = score(backup_x, backup_y)
-        new_score = score(x, y)
+        # old_score = score(backup_x, backup_y)
+        # new_score = score(x, y)
 
-        if new_score > old_score:
+        if score > old_score:
             x = backup_x
             y = backup_y
 
         scores.append(old_score)
 
-        if old_score < lowest_score:
+        if score < lowest_score:
             best_x = copy.deepcopy(x)
             best_y = copy.deepcopy(y)
-            lowest_score = copy.deepcopy(old_score)
+            lowest_score = copy.deepcopy(score)
 
 
         rotations += 1
@@ -77,55 +80,96 @@ def fold_protein(x, y, amino_pos):
 
     direction = random.random()
 
+    score = 0
     for i in range(amino_pos + 1, LENGTH):
         relative_x = x[i] - rotating_amino_x
         relative_y = y[i] - rotating_amino_y
-
+        
         if direction > 0.5:
             # rotate left
             x[i] = rotating_amino_x - relative_y
             y[i] = rotating_amino_y + relative_x
+            
         else:
             # rotate right
             x[i] = rotating_amino_x + relative_y
             y[i] = rotating_amino_y - relative_x
 
-
-def score(x, y):
-    coordinates = []
-    score = 0
-
-    for i in range(LENGTH):
-
-        if not PROTEIN[i] == 'P':
-            
-            for direction in DIRECTIONS:
-                dir_x = direction[0]
-                dir_y = direction[1]
-                check_x = x[i] + dir_x
-                check_y = y[i] + dir_y
-
-                for j in range(len(coordinates)):
-
-                    if [check_x, check_y] == coordinates[j] and not \
-                        (check_x == x[i - 1] and check_y == y[i - 1]):
-
-                        protein = PROTEIN[i]
-                        neighbour = PROTEIN[j]
-
-                        if protein == 'H':
-                            if neighbour == 'H' or neighbour == 'C':
-                                score += -1
-
-                        if protein == 'C':
-                            if neighbour == 'C':
-                                score += -5
-                            if neighbour == 'H':
-                                score += -1
-
-        coordinates.append([x[i], y[i]])
+        if i == amino_pos + 1 and PROTEIN[i] != "P":
+            score = check_score(x, y, rotating_amino_x, rotating_amino_y, x[i], y[i], PROTEIN[i]) 
 
     return score
+
+
+def check_score(x, y, previous_x, previous_y, amino_x, amino_y, amino):
+    protein_coords = list(zip(x, y))   
+    score = 0
+
+    for direction in DIRECTIONS:
+
+        dir_x = direction[0]
+        dir_y = direction[1]
+        check_x = amino_x + dir_x
+        check_y = amino_y + dir_y
+        neighbour_coords = (check_x, check_y)
+
+        if neighbour_coords in protein_coords and not (check_x == previous_x and check_y == previous_y):
+            # print(check_x, check_y)
+            # print(protein_coords.index(neighbour_coords))
+            neighbour_index = protein_coords.index(neighbour_coords)
+            
+            neighbour = PROTEIN[neighbour_index]
+
+            if amino == "H":
+                if neighbour == "H" or neighbour == "C":
+                    score += -1
+
+            if amino == "C":
+                if neighbour == "H":
+                    score += -1
+                if neighbour == "C":
+                    score += -5
+
+    return score
+            
+            
+
+            
+# def score(x, y):
+#     coordinates = []
+#     score = 0
+
+#     for i in range(LENGTH):
+
+#         if not PROTEIN[i] == 'P':
+            
+#             for direction in DIRECTIONS:
+#                 dir_x = direction[0]
+#                 dir_y = direction[1]
+#                 check_x = x[i] + dir_x
+#                 check_y = y[i] + dir_y
+
+#                 for j in range(len(coordinates)):
+
+#                     if [check_x, check_y] == coordinates[j] and not \
+#                         (check_x == x[i - 1] and check_y == y[i - 1]):
+
+#                         protein = PROTEIN[i]
+#                         neighbour = PROTEIN[j]
+
+#                         if protein == 'H':
+#                             if neighbour == 'H' or neighbour == 'C':
+#                                 score += -1
+
+#                         if protein == 'C':
+#                             if neighbour == 'C':
+#                                 score += -5
+#                             if neighbour == 'H':
+#                                 score += -1
+
+#         coordinates.append([x[i], y[i]])
+
+#     return score
 
 def visualization(x, y, score, scores):
 
@@ -153,25 +197,33 @@ def visualization(x, y, score, scores):
             C_x.append(amino_x)
             C_y.append(amino_y)
 
-    plt.figure()
 
-    plt.subplot(211)
-    plt.plot(x, y, "k--")
-    plt.plot(H_x, H_y, "ro", label = "hydrofoob")
-    plt.plot(P_x, P_y, "bo", label = "polair")
-    plt.plot(C_x, C_y, "go", label = "cysteine")
-    plt.xlabel("step")
-    plt.ylabel("step")
-    plt.legend()
-    plt.title("Total score: {}".format(score))
-
-    plt.subplot(212)
-    plt.plot(iterations, scores)
-    plt.title("Statistics")
-    plt.xlabel("iterations")
-    plt.ylabel("scores")
-    plt.savefig("hillclimber.png")
+        plt.plot(H_x, H_y, "ro", label = "hydrofoob")
+        plt.plot(P_x, P_y, "bo", label = "polair")
+        plt.plot(C_x, C_y, "go", label = "cysteine")
+        plt.xlabel("step")
+        plt.ylabel("step")
     plt.show()
+
+    # plt.figure()
+
+    # plt.subplot(211)
+    # plt.plot(x, y, "k--")
+    # plt.plot(H_x, H_y, "ro", label = "hydrofoob")
+    # plt.plot(P_x, P_y, "bo", label = "polair")
+    # plt.plot(C_x, C_y, "go", label = "cysteine")
+    # plt.xlabel("step")
+    # plt.ylabel("step")
+    # plt.legend()
+    # plt.title("Total score: {}".format(score))
+
+    # plt.subplot(212)
+    # plt.plot(iterations, scores)
+    # plt.title("Statistics")
+    # plt.xlabel("iterations")
+    # plt.ylabel("scores")
+    # plt.savefig("hillclimber.png")
+    # plt.show()
 
 
 if __name__ == "__main__":
